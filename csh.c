d022638687f9056189a6885aae3103c70e4f9e0d
Nikhil Pinnaparaju <nikhilpinnaparaju@gmail.com>
2017-08-27 17:04:54 +0530

    Name Works

--- csh.c
+++ csh.c
@@ -6,16 +6,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <assert.h>
 
-char buffer[1000];
+char workingDir[1000];
 char host[1000];
+char logged_in[1000];
 
 int main()
 {
-    getcwd(buffer,1000);
-    gethostname(host,1000);
+    char *found, *start;
 
-    printf("%s %s\n",buffer,host);
+    start = workingDir;
+
+    getcwd(workingDir,1000);
+    gethostname(host,1000);
+    
+    // printf("%s\n",start);
+    int count = 0;
+
+    while ( (found = strsep(&start,"/")) != NULL)
+    {
+        // printf("%d:%s\n",count,found);
+
+        if (count == 2)
+        {
+            strcpy(logged_in,found);
+        }
+
+        count++;
+    }
+
+    printf("%s@%s>%s\n",logged_in,host,workingDir);
 
     return 0;
 }
